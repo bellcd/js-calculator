6a50ca321fec87d3cd2961287ab2d6c2b36ce2c5
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-09-24 12:18:34 -0400

    Updates

---
+++ precache-manifest.09d638c910440eed5cd6a3af2dcc7b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15c5062259c5b0fadfb8d4a66e45ad3",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "cd4528d8e98e3658443e",
+    "url": "/js-calculator/static/css/main.28407630.chunk.css"
+  },
+  {
+    "revision": "dc6c74cf798ed42bdc64",
+    "url": "/js-calculator/static/js/2.d091e519.chunk.js"
+  },
+  {
+    "revision": "cd4528d8e98e3658443e",
+    "url": "/js-calculator/static/js/main.28b42684.chunk.js"
+  },
+  {
+    "revision": "96c35b40fe53ed838f2a",
+    "url": "/js-calculator/static/js/runtime-main.6eb0257e.js"
+  }
+]);
